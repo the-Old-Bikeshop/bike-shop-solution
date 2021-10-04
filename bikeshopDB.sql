072de0ca7f2eebe58d25fffdf0223e6495f372a1
darthradek <radoslawhaller@gmail.com>
2021-10-04 11:39:32 +0200

    it works finally

--- bikeshopDB.sql
+++ bikeshopDB.sql
@@ -2,36 +2,30 @@ DROP DATABASE IF EXISTS bikeshop;
 CREATE DATABASE bikeshop;
 USE bikeshop;
 
-CREATE TABLE user (
-  userID INT NOT NULL PRIMARY KEY
-  nickname VARCHAR(100) NOT NULL,
+CREATE TABLE `user` (
+  userID INT NOT NULL PRIMARY KEY,
+  nick_name VARCHAR(100) NOT NULL,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
   password_hash VARCHAR(155) NOT NULL,
   email VARCHAR(100) NOT NULL,
   phone_number VARCHAR(100) NOT NULL,
-  `role` INT NOT NULL,
-  invoice_addressID INT NOT NULL,
-  delivery_addressID INT NOT NULL,
-  FOREIGN KEY (invoice_addressID) REFERENCES `address` ( addressID ),
-  FOREIGN KEY (delivery_addressID) REFERENCES `address` ( addressID )
+  `role` VARCHAR(100) NOT NULL
+);
+CREATE TABLE city (
+  postalCodeID INT NOT NULL PRIMARY KEY,
+  `name` VARCHAR(100) NOT NULL
 );
 CREATE TABLE `address` (
   addressID INT NOT NULL PRIMARY KEY,
   street_name VARCHAR(150) NOT NULL,
   address_content VARCHAR(150) NOT NULL,
   phone_number VARCHAR(100) NOT NULL,
-  address_type INT NOT NULL,
-  postalCodeID INT NOT NULL,
-  FOREIGN KEY (postalCodeID) REFERENCES city (postalCodeID),
-);
-CREATE TABLE favourite_products(
+  address_type VARCHAR(100) NOT NULL,
   userID INT NOT NULL,
-  productID INT NOT NULL,
-  FOREIGN KEY (userID) REFERENCES user (userID),
-  FOREIGN KEY (productID) REFERENCES product (productID),
-  CONSTRAINT PK_favourite_products PRIMARY KEY(userID, productID)
+  postalCodeID INT NOT NULL,
+  FOREIGN KEY (userID) REFERENCES `user` ( userID ),
+  FOREIGN KEY (postalCodeID) REFERENCES city (postalCodeID)
 );
 CREATE TABLE shipping (
   shippingID INT NOT NULL PRIMARY KEY,
@@ -41,19 +35,15 @@ CREATE TABLE shipping (
   userID INT NOT NULL,
   FOREIGN KEY (userID) REFERENCES user (userID)
 );
-CREATE TABLE city (
-  postalCodeID INT NOT NULL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-);
 CREATE TABLE review (
   reviewID INT NOT NULL PRIMARY KEY,
-  creation_date VARCHAR(100)
+  creation_date VARCHAR(100),
   title VARCHAR(255),
   content TEXT NOT NULL,
   userID INT NOT NULL,
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
-CREATE TABLE order (
+CREATE TABLE `order` (
   orderID INT NOT NULL PRIMARY KEY,
   creation_date VARCHAR(100),
   `status` VARCHAR(100) NOT NULL,
@@ -61,22 +51,9 @@ CREATE TABLE order (
   total_price INT NOT NULL,
   purchase_order_number INT NOT NULL,
   userID INT NOT NULL,
-  delivery_methodID INT NOT NULL,
-  invoice_addressID INT NOT NULL,
-  delivery_addressID INT NOT NULL,
-  FOREIGN KEY (userID) REFERENCES user (userID),
-  FOREIGN KEY (shippingID) REFERENCES shipping (shippingID),
-  FOREIGN KEY (invoice_addressID) REFERENCES `address` ( addressID ),
-  FOREIGN KEY (delivery_addressID) REFERENCES `address` ( addressID )
-);
-CREATE TABLE order_products(
-  quantity INT NOT NULL,
-  paid_price INT NOT NULL,
-  userID INT NOT NULL,
-  productID INT NOT NULL,
-  FOREIGN KEY (userID) REFERENCES user (userID),
-  FOREIGN KEY (productID) REFERENCES product (productID),
-  CONSTRAINT PK_order_products PRIMARY KEY(userID, productID)
+  shippingID INT NOT NULL,
+  FOREIGN KEY (userID) REFERENCES `user` (userID),
+  FOREIGN KEY (shippingID) REFERENCES shipping (shippingID)
 );
 CREATE TABLE drive_type (
   drive_typeID INT NOT NULL PRIMARY KEY,
@@ -130,7 +107,7 @@ CREATE TABLE bike_specifications (
   FOREIGN KEY (wheel_sizeID) REFERENCES wheel_size (wheel_sizeID),
   FOREIGN KEY (braking_systemID) REFERENCES braking_system (braking_systemID),
   FOREIGN KEY (drive_typeID) REFERENCES drive_type (drive_typeID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
 CREATE TABLE product (
   productID INT NOT NULL PRIMARY KEY,
@@ -149,7 +126,23 @@ CREATE TABLE product (
   userID INT NOT NULL,
   FOREIGN KEY (bike_specificationsID) REFERENCES bike_specifications (bike_specificationsID),
   FOREIGN KEY (brandID) REFERENCES brand (brandID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
+);
+CREATE TABLE order_products(
+  quantity INT NOT NULL,
+  paid_price INT NOT NULL,
+  userID INT NOT NULL,
+  productID INT NOT NULL,
+  FOREIGN KEY (userID) REFERENCES `user` (userID),
+  FOREIGN KEY (productID) REFERENCES product (productID),
+  CONSTRAINT PK_order_products PRIMARY KEY(userID, productID)
+);
+CREATE TABLE favourite_products(
+  userID INT NOT NULL,
+  productID INT NOT NULL,
+  FOREIGN KEY (userID) REFERENCES user (userID),
+  FOREIGN KEY (productID) REFERENCES product (productID),
+  CONSTRAINT PK_favourite_products PRIMARY KEY(userID, productID)
 );
 CREATE TABLE product_list(
   ID INT NOT NULL,
@@ -173,7 +166,7 @@ CREATE TABLE product_has_images (
   FOREIGN KEY (productID) REFERENCES product (productID),
   FOREIGN KEY (imageID) REFERENCES `image`(imageID),
   CONSTRAINT PK_product_has_images PRIMARY KEY (productID, imageID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
 CREATE TABLE post (
   postID INT NOT NULL PRIMARY KEY,
@@ -183,7 +176,7 @@ CREATE TABLE post (
   productID INT NOT NULL,
   userID INT NOT NULL,
   FOREIGN KEY (productID) REFERENCES product (productID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
 CREATE TABLE post_has_images (
   postID INT NOT NULL,
@@ -200,7 +193,7 @@ CREATE TABLE comment (
   userID INT NOT NULL,
   postID INT NOT NULL,
   FOREIGN KEY (postID) REFERENCES post(postID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
 CREATE TABLE reply (
   replyID INT NOT NULL PRIMARY KEY,
@@ -210,7 +203,7 @@ CREATE TABLE reply (
   userID INT NOT NULL,
   commentID INT NOT NULL,
   FOREIGN KEY (commentID) REFERENCES comment(commentID),
-  FOREIGN KEY (userID) REFERENCES user (userID)
+  FOREIGN KEY (userID) REFERENCES `user` (userID)
 );
 CREATE TABLE `page` (
   pageID INT NOT NULL PRIMARY KEY,
