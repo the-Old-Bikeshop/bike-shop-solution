5dbbd116823cb6038a936b3fccacce2209a7e5a8
raul-octavian <alburaul@gmail.com>
2021-10-05 14:38:33 +0200

    added notes on what to make as classes and fucntions, it is preliminary.

---
+++ documentation/classes/notes.php
@@ -0,0 +1,67 @@
+parts class
+        -functions:
+
+            add/get/update drive type
+            add/get/update wheel size
+            add/get/update brand
+            add/get/update category
+            add/get/update braking system
+
+image class
+
+        -functions:
+            check format
+            resize
+            save
+            add to database
+        return db position
+
+bike specks
+
+        use parts class
+                get parts
+
+        add/get/update bike specks
+
+product class
+            use parts class
+            get parts
+            use image class
+            get all products
+            add/get/update product
+
+
+user class
+            use image class
+            add/get/update/view user
+            add/get/update address
+            add/remove and view liked products
+            view all products
+                    -contact store about bought products(email)
+
+            add/remove review
+
+Order class
+
+            add/get/update/view delivery methods
+            add order, update sesion and cookie
+
+post class
+            use image class
+            add/update and view all posts
+            add/remove/update comment if user is author
+            show all comments
+            show new posts(make a order in the call)
+
+
+
+
+
+
+
+
+
+
+
+
+
