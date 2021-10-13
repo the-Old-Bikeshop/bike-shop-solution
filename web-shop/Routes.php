871cd25bb7e77d5ca58ac9bb8733bdecc50ae067
darthradek <radoslawhaller@gmail.com>
2021-10-13 21:28:40 +0200

    model and view implementation

---
+++ web-shop/Routes.php
@@ -0,0 +1,11 @@
+<?php 
+
+Route::set('about-us', function() {
+    AboutUs::CreateView('AboutUs');
+});
+
+Route::set('contact-us', function() {
+    ContactUs::CreateView('ContactUs');
+});
+
+?>
---
+++ web-shop/controllers/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class Controller {
+
+    public static function CreateView($viewName) {
+        require_once("./views/$viewName.php");
+    }
+    
+}
+
+?>
