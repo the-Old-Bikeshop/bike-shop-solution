86b35ce06d3d8ef8d1d3b9df747299a91dc27d1c
darthradek <radoslawhaller@gmail.com>
2021-10-16 21:19:34 +0200

    no message

--- web-shop/controllers/ViewController.php
+++ web-shop/controllers/ViewController.php
@@ -3,6 +3,7 @@
 class ViewController {
 
     public static function CreateView($viewName) {
+        require_once("./views/admin/$viewName.php");
         require_once("./views/$viewName.php");
     }
     
--- web-shop/index.php
+++ web-shop/index.php
@@ -6,8 +6,11 @@
         if (file_exists('./controllers/'.$class_name.'.php')) {
 
             require_once './controllers/'.$class_name.'.php';
+           
+        } elseif (file_exists('./controllers/admin/'.$class_name.'.php')) {
 
-        }
+            require_once './controllers/admin/'.$class_name.'.php';
+        };
 
     }
     
