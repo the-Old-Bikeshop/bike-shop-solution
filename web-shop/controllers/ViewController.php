b329e6d3be839f8b1010a2df0002d6fcb4ba0191
raul-octavian <alburaul@gmail.com>
2021-10-18 10:52:53 +0200

    refactored view controller

--- web-shop/controllers/ViewController.php
+++ web-shop/controllers/ViewController.php
@@ -6,14 +6,44 @@ class ViewController {
 
 
     public static function CreateView($viewName) {
+        $dirs = array(
+            'views/',
+            'views/admin/',
+        );
 
-        if (file_exists('./views/'.$viewName.'.php')) {
-            require_once './views/'.$viewName.'.php';
-        } elseif (file_exists('./views/admin/'.$viewName.'.php')) {
-            require_once './views/admin/'.$viewName.'.php';
+        foreach( $dirs as $dir ) {
+            if (file_exists('./'. $dir . $viewName .'.php')) {
+                require_once('./'. $dir . $viewName .'.php');
+                return;
+            }
         }
+        
+//        if (file_exists('./views/'.$viewName.'.php')) {
+//            require_once './views/'.$viewName.'.php';
+//        } elseif (file_exists('./views/admin/'.$viewName.'.php')) {
+//            require_once './views/admin/'.$viewName.'.php';
+//        }
     }
 
 }
+
+// CreateView($viewName) {
+//
+//    // Define an array of directories in the order of their priority to iterate through.
+//    $dirs = array(
+//        'models/',
+//        'controllers/',
+//        'controllers/admin',
+//    );
+//
+//    // Looping through each directory to load all the class files. It will only require a file once.
+//    // If it finds the same class in a directory later on, IT WILL IGNORE IT! Because of that require once!
+//    foreach( $dirs as $dir ) {
+//        if (file_exists($dir . $class_name .'.php')) {
+//            require_once($dir . $class_name.'.php');
+//            return;
+//        }
+//    }
+//});
 
 ?>
--- web-shop/views/AboutView.php
+++ web-shop/views/AboutView.php
@@ -6,6 +6,9 @@ $about = new AboutController();
 var_dump($about->user);
 
 
+$routs  = new RouterController();
+var_dump($routs::$validRoutes);
+
 ?>
 
 <h1>the about us</h1>
