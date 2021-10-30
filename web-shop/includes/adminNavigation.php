112de33832fd848ef499f0b96b8d8cc7fc873ece
darthradek <radoslawhaller@gmail.com>
2021-10-30 18:48:34 +0200

    Adding admin nav tabs and css

--- web-shop/includes/adminNavigation.php
+++ web-shop/includes/adminNavigation.php
@@ -1,17 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Admin</title>
-</head>
-<body>
-    <div>
-        <p>Cipka mnaim</p>
+    <div class="tab_nav_wrapper" style="width: 20vw; height:100vh; background-color:whitesmoke; border-right:rgba(0,0,0,0.10) solid 0.5px;">
+        <div class="tab_nav_container" style="padding:1rem;width:100%; height:100%; display:flex; flex-direction:column; justify-content:flex-start; align-items:center;">
+                <div style="display: flex; justify-content:flex-start; align-items:center; height:auto; width:100%;">
+                    <img style="width: 4rem; height:4rem;" src="/bike-shop-solution/web-shop/public/owl-logo.svg" alt="">
+                </div>
+                <div style="width: 100%; margin-top:8rem;">
+                    <div style="width: 100% !important;" class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link active" id="v-pills-dashboard-tab" data-toggle="pill" href="#v-pills-dashboard" role="tab" aria-controls="v-pills-dashboard" aria-selected="true">Dashboard</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-products-tab" data-toggle="pill" href="#v-pills-products" role="tab" aria-controls="v-pills-products" aria-selected="false">Products</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-categories-tab" data-toggle="pill" href="#v-pills-categories" role="tab" aria-controls="v-pills-categories" aria-selected="false">Categories</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-brands-tab" data-toggle="pill" href="#v-pills-brands" role="tab" aria-controls="v-pills-brands" aria-selected="false">Brands</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-wheel-drive-types-tab" data-toggle="pill" href="#v-pills-drive-types" role="tab" aria-controls="v-pills-drive-types" aria-selected="false">Drive Types</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-wheel-break-system-tab" data-toggle="pill" href="#v-pills-break-system" role="tab" aria-controls="v-pills-break-system" aria-selected="false">Break Systems</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-wheel-sizes-tab" data-toggle="pill" href="#v-pills-wheel-sizes" role="tab" aria-controls="v-pills-wheel-sizes" aria-selected="false">Wheel Sizes</a>
+                        <a style="font-size:1.2rem; width: 100% !important;" class="nav-link" id="v-pills-users-tab" data-toggle="pill" href="#v-pills-users" role="tab" aria-controls="v-pills-users" aria-selected="false">Users</a>
+                    </div>
+                </div>
+                <!-- <div class="col-9">
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                    </div>
+                </div> -->
+
+            <div style="height:7vh; width: 20vw; position:absolute; bottom:0; left:0; display:flex; align-items:center; padding:1rem; justify-content:flex-start; background-color:rgba(0,0,0,0.15);">
+                <h4 style="color:black; opacity:0.4; font-size:0.8rem; margin:0; padding:0;">Logged in user: Radek or Raul</h4>
+            </div>
+        </div>
     </div>
-
-
-
--- web-shop/views/admin/DriveTypeView.php
+++ web-shop/views/admin/DriveTypeView.php
@@ -4,53 +4,55 @@ $dt = new DriveTypeController();
 $dt->setDriveType();
 $dt->getDriveType();
 ?>
-
-
-<?php include include(dirname(__DIR__).'/includes/adminNavigation.php');?>
-
-<section class="row row col-12 row mt-5 col-md-8 offset-md-2 pb-5">
-    <div class="col-12 col-md-12 offset-md-2 mt-5 mb-3 ">
-        <h2>
-            Drive Type
-        </h2>
+<section class="row col-12">
+    <div style=" align-items:center; justify-content:space-between; border-bottom:2px dashed rgba(0,0,0,0.15); padding:1rem; width:100%; display: flex;">
+        <h1>
+            Drive Types
+        </h1>
+        <button data-toggle="modal" data-target="#exampleModalCenter" style="height: 3rem;" type="button" class="btn btn-success">
+            Create New
+        </button>
     </div>
-    <h3 class="col-12 col-md-8 offset-md-2">All drive types</h3>
-    <table class="table table-sm col-12 col-md-8 offset-md-2 pb-5 border-bottom border-secondary">
-        <thead class="thead-light">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Short description</th>
-            <th scope="col">Description</th>
-            <th scope="col">Controls</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($dt->getDriveType()->fetchAllDriveTypes() as $res): ?>
-            <tr>
-                <th scope="row"> <?php echo $res["drive_typeID"] ?></th>
-                <td><?php echo $res['name']?></td>
-                <td><?php echo $res['short_description']?></td>
-                <td><?php echo $res['description']?></td>
-                <td>
-                    <form action="" method="post" class="d-inline-block">
-                        <input type="text" hidden name="drive_typeID" value="<?php echo $res['drive_typeID'] ?>">
-                        <input type="submit" name="update" value="update" class="btn btn-info" >
-                    </form>
-                    <form action="" method="post" class="d-inline-block">
-                        <input type="text" hidden name="drive_typeID" value="<?php echo $res['drive_typeID'] ?>">
-                        <input type="submit" name="delete" value="delete" class="btn btn-danger" onclick="return confirm('Delete! are you sure?')" >
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach ?>
+    <div style="padding-left:1rem; padding:1rem; width:100%;">
+        <h3 style="padding: 0; margin-top:0.5rem;" class="col-12">All drive types</h3>
+        <table class="table table-sm col-12 col-md-8 pb-5 border-bottom border-secondary">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Short description</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Controls</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($dt->getDriveType()->fetchAllDriveTypes() as $res) : ?>
+                    <tr>
+                        <th scope="row"> <?php echo $res["drive_typeID"] ?></th>
+                        <td><?php echo $res['name'] ?></td>
+                        <td><?php echo $res['short_description'] ?></td>
+                        <td><?php echo $res['description'] ?></td>
+                        <td>
+                            <form action="" method="post" class="d-inline-block">
+                                <input type="text" hidden name="drive_typeID" value="<?php echo $res['drive_typeID'] ?>">
+                                <input type="submit" name="update" value="update" class="btn btn-info">
+                            </form>
+                            <form action="" method="post" class="d-inline-block">
+                                <input type="text" hidden name="drive_typeID" value="<?php echo $res['drive_typeID'] ?>">
+                                <input type="submit" name="delete" value="delete" class="btn btn-danger" onclick="return confirm('Delete! are you sure?')">
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
+
 
 
     <!--    display message-->
-    <?php if(isset($dt->getDriveType()->message)): ?>
+    <?php if (isset($dt->getDriveType()->message)) : ?>
 
         <div class="col-12 col-md-8 offset-md-2">
             <h3><?php echo $dt->getDriveType()->message ?></h3>
@@ -60,47 +62,47 @@ $dt->getDriveType();
 
 
     <!--    the form for creating and updating drive_type starts here-->
-
-
-
-    <form class="col-12 row mt-5 col-12 col-md-8 offset-md-2 pb-5" action="" method="post" id="form">
-        <p class="text-secondary  col-12"><?php echo !$dt->getUpdate() ? "Create new drive type" : "Update product " . $dt->getDrive()['name'] ?> </p>
-        <div class="form-group col-12 mt-2">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name"
-                   placeholder="name of the drive type"
-                   value=" <?php echo $dt->getDrive()['name'] ?? '' ?>" required>
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="col-12" action="" method="post" id="form">
+                        <p class="text-secondary col-12"><?php echo !$dt->getUpdate() ? "Create new drive type" : "Update product " . $dt->getDrive()['name'] ?> </p>
+                        <div class="form-group col-12 mt-2">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="name of the drive type" value=" <?php echo $dt->getDrive()['name'] ?? '' ?>" required>
+                        </div>
+                        <div class="form-group col-12  mt-2">
+                            <label for="short_description">Short description(max 255 characters)</label>
+                            <input type="text" class="form-control" id="short_description" name="short_description" placeholder="short description 255 characters" value="<?php echo $dt->getDrive()['short_description'] ?? '' ?>"">
         </div>
-        <div class="form-group col-12  mt-2">
-            <label for="short_description">Short description(max 255 characters)</label>
-            <input type="text" class="form-control" id="short_description" name="short_description"
-                   placeholder="short description 255 characters"
-                   value="<?php echo $dt->getDrive()['short_description'] ?? ''?>"">
+        <div class=" form-group col-12 mt-2">
+                            <label for="description">Describe products in detail</label>
+                            <textarea class="form-control" id="description" rows="5" name="description"><?php echo $dt->getDrive()['description'] ?? '' ?></textarea>
+                        </div>
+                        <?php if (isset($dt->getDrive()['drive_typeID'])) : ?>
+                            <input type="text" hidden name="drive_typeID" value="<?php echo $dt->getDrive()['drive_typeID'] ?>">
+                        <?php endif; ?>
+                        <div class="form-group col-12 mt-2">
+                            <input type="submit" class="btn <?php echo !$dt->getUpdate() ? 'btn-primary' : 'btn-info' ?>" name="<?php echo !$dt->getUpdate() ? 'submit-new' : 'submit-update' ?>" value="<?php echo !$dt->getUpdate() ? 'Create new' : 'update' ?>">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="form-group col-12  mt-2">
-            <label for="description">Describe products in detail</label>
-            <textarea class="form-control"
-                      id="description" rows="5"
-                      name="description"><?php echo $dt->getDrive()['description'] ?? ''?></textarea>
-        </div>
-        <?php if(isset($dt->getDrive()['drive_typeID'])): ?>
-
-            <input type="text" hidden
-                   name = "drive_typeID"
-                   value = "<?php echo $dt->getDrive()['drive_typeID'] ?>">
-
-        <?php endif; ?>
-        <div class="form-group col-12 mt-2">
-            <input type="submit" class="btn <?php echo !$dt->getUpdate() ? 'btn-primary' : 'btn-info' ?>"
-                   name="<?php echo !$dt->getUpdate() ? 'submit-new' : 'submit-update' ?>"
-                   value="<?php echo !$dt->getUpdate() ? 'Create new' : 'update' ?>">
-        </div>
-
-    </form>
+    </div>
 
 </section>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+
 </html>
