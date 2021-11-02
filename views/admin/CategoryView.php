7c03985601600a0e3c21bac5f62560f2450bd987
darthradek <radoslawhaller@gmail.com>
2021-11-02 19:06:30 +0100

    Updating admin navigation styles

--- views/admin/CategoryView.php
+++ views/admin/CategoryView.php
@@ -13,7 +13,7 @@
         <h1>
             Categories
         </h1>
-        <button data-toggle="modal" data-target="#exampleModalCenter" style="height: 3rem;" type="button" class="btn btn-primary">
+        <button data-toggle="modal" data-target="#exampleModalCenter" style="height: 3rem;" type="button" class="btn btn-dark">
             Create New
         </button>
     </div>
