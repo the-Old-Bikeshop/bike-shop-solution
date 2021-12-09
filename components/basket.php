4f21244c794442c1ca0a2af780e8ebc231e0e26e
raul-octavian <alburaul@gmail.com>
2021-12-09 06:21:26 +0100

    basket start

---
+++ components/basket.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+
+<div class="card">
+    <div class="product-container">
+        <div class="product-wrapper">
+            <!--            loop start here-->
+            <div class="product">
+                <div class="product__image">
+                    <img
+                            src=""
+                            alt=""
+                            width="300"
+                            height="300">
+                </div>
+                <div class="product__info">
+                    <div class="product-info__name">
+                        <h2>name</h2>
+                    </div>
+                    <div class="product-info__math">
+                        <div>
+                            <div class="product-info__gty">
+                                <label for="quantity"></label>
+                                <input type="number" value="1">
+                            </div>
+                            <div class="product-info__price">
+                                Price: 1000.00
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <!--            loop ends here-->
+
+        </div>
+
+    </div>
+
+    <div class="card-footer">
+        <div class="delivery">
+            Delivery : 0
+        </div>
+        <div class="subtotal">
+            Subtotal: 750
+        </div>
+        <div class="tax">
+            Tax: 250
+        </div>
+        <div class="totalPrice">
+            Total price: 1000
+        </div>
+
+    </div>
+
+</div>
